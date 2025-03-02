55a57e9038771fc2bab38469943fc7de27e78b3d
Yii-OvO <1535487179@qq.com>
2025-03-02 17:02:13 +0800

    添加角色的RUD接口_修改gtoken相关的部分代码

--- api/backend/login.go
+++ api/backend/login.go
@@ -7,7 +7,7 @@ import (
 )
 
 type LoginDoReq struct {
-	g.Meta   `path:"/backend/login" method:"post" tags:"登录" summary:"执行登录请求"`
+	//g.Meta   `path:"/backend/login" method:"post" tags:"登录" summary:"执行登录请求"`
 	Name     string `json:"name" v:"required#请输入账号"   dc:"账号"`
 	Password string `json:"password" v:"required#请输入密码"   dc:"密码(明文)"`
 }
--- api/backend/role.go
+++ api/backend/role.go
@@ -11,3 +11,31 @@ type RoleReq struct {
 type RoleRes struct {
 	RoleId int `json:"role_id"`
 }
+
+type RoleUpdateReq struct {
+	g.Meta `path:"/backend/role/update" method:"post" tags:"角色" sm:"修改角色"`
+	Id     uint   `json:"id"   v:"required#角色Id不能为空" dc:"角色Id"`
+	Name   string `json:"name" v:"required#角色名称不能为空" dc:"角色名称"`
+	Desc   string `json:"desc" dc:"描述"`
+}
+
+type RoleUpdateRes struct {
+	RoleId uint `json:"id"`
+}
+
+type RoleDeleteReq struct {
+	g.Meta `path:"/backend/role/delete" method:"delete" tags:"角色" summary:"删除角色"`
+	Id     uint `v:"min:1#请选择需要删除的角色" dc:"角色id"`
+}
+type RoleDeleteRes struct{}
+
+type RoleGetListCommonReq struct {
+	g.Meta `path:"/backend/role/list" method:"get" tags:"角色" summary:"角色列表"`
+	CommonPaginationReq
+}
+type RoleGetListCommonRes struct {
+	List  interface{} `json:"list" description:"列表"`
+	Page  int         `json:"page" description:"分页码"`
+	Size  int         `json:"size" description:"分页数量"`
+	Total int         `json:"total" description:"数据总数"`
+}
